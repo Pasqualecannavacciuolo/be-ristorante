a4f9fcaf22bdce01d9ae62e6a5df5dc28c34b94f
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2023-05-19 18:49:23 +0200

    Initial commit

---
+++ src/test/java/com/ristorante/beristorante/BeRistoranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ristorante.beristorante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeRistoranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
